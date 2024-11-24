d112a78c7842f9fe4c9aa367cf638c6dfa0f01d9
lukemccrae <lukemccrae@gmail.com>
2024-11-23 21:58:15 -0800

    demo

--- src/Details.tsx
+++ src/Details.tsx
@@ -33,6 +33,47 @@ export const Details = () => {
     }
   }, [id, user]);
 
+  function haversine(lat1: number, lon1: number, lat2: number, lon2: number): number {
+    const toRadians = (degree: number) => degree * (Math.PI / 180);
+
+    const R = 6371; // Radius of the Earth in kilometers
+    const dLat = toRadians(lat2 - lat1);
+    const dLon = toRadians(lon2 - lon1);
+
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c; // Distance in kilometers
+}
+
+  const length = (map: number[][]) => {
+    let distance = 0;
+    let i = 500;
+    
+    while(i < map.length - 2) {
+      console.log(distance)
+      let lat1: number = map[i][0]
+      let lat2: number = map[i][1]
+      let long1: number = map[i + 1][0]
+      let long2: number = map[i + 1][1]
+
+      
+      distance += haversine(lat1, long1, lat2, long2)
+    }
+    return distance;
+  }
+
+  const shorten = (map: number[]) => {
+    console.log(map, '<< map')
+    // shorten points
+    // return map;
+    return 5;
+  }
+
   if (plan) {
     return (
       <Box
@@ -100,6 +141,26 @@ export const Details = () => {
               alignItems: 'flex-start',
             }}
           >
+          <div>Points: {map?.length}</div>
+          <div>Length:4 {length(map as number [][])}</div>
+
+            <MapComponent map={map}></MapComponent>
+          </Box>
+
+
+          <Box
+            sx={{
+              flex: 1,
+              backgroundColor: '#e3e3e3',
+              borderRadius: 2,
+              padding: 2,
+              justifyContent: 'center',
+              alignItems: 'flex-start',
+            }}
+          >
+            <div>Points: {map?.length}</div>
+            {/* <div>Length: {shorten(map)}</div> */}
+
             <MapComponent map={map}></MapComponent>
           </Box>
         </Grid>
