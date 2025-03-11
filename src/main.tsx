a05d96cbcebe01bc2ebc8fe42d9a8f4048b2c6d1
lukemccrae <lukemccrae@gmail.com>
2025-03-10 23:06:19 -0700

    sigh

--- src/main.tsx
+++ src/main.tsx
@@ -19,11 +19,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Dashboard></Dashboard>
       <Routes>
         <Route path="/" element={<Articles />}></Route>
-        {/* <Route path="/app" element={<App />}></Route> */}
+        <Route path="/app" element={<App />}></Route>
         <Route path="/users/:username" element={<Profile />}></Route>
         <Route path="/users/:username/posts/:slug" element={<ArticlePage />}></Route>
-        {/* <Route path="/add-course" element={<AddCourse />}></Route> */}
-        {/* <Route path="/app/course/:slug" element={<Details />}></Route> */}
+        <Route path="/add-course" element={<AddCourse />}></Route>
+        <Route path="/app/course/:slug" element={<Details />}></Route>
       </Routes>
       </UserProvider>
       <Footer></Footer>
